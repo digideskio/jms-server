4fff6b804954452409eae9649ccf520699690337
_nec <nec@shell8.net>
2015-08-27 09:58:10 +0200

    cleanup

--- hooks/npm.postinstall.js
+++ hooks/npm.postinstall.js
@@ -4,16 +4,10 @@ var k = path.normalize(__dirname + '/../..');
 
 if ('node_modules' !== k.split(path.sep).pop()) return;
 
-var p='../plugins',
-	l='../lib',
+var l='../lib',
 	dl='node_modules/lib',
-	dp='node_modules/plugins',
 	fs=require('fs');
 
 fs.exists(l, function(e) {
 	e || fs.symlinkSync(l, dl, 'dir')
-});
-
-fs.exists(p, function(e) {
-	e || fs.symlinkSync(p, dp, 'dir')
 });
