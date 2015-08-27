85adf4528a0cd0987b9876fc1041bdf05928987b
_nec <nec@shell8.net>
2015-08-27 14:55:19 +0200

    put back errbit

--- bin/jms.js
+++ bin/jms.js
@@ -1,5 +1,12 @@
 //var heapdump = require('heapdump');
 var config = require('jms-config');
+var airbrake = require('airbrake').createClient(config.debug.errbit.apiKey, config.debug.errbit.env);
+
+airbrake.serviceHost = config.debug.errbit.host;
+airbrake.protocol = config.debug.errbit.protocol;
+
+airbrake.handleExceptions();
+
 
 var server = require('../server')(config);
 
